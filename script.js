6da5cff214beb54bbbcd9045d08e14ce35bd0d3a
kevinmundo <kevinmundo99@gmail.com>
2020-10-27 14:38:54 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+var foto;
+window.onload = function() {
+  
+  foto = new Foto();
+}
+
+function selectImage(){
+  document.getElementById("foto-file").click();
+}
+
+function makeGrayScale() {
+  foto.grayscale()
+}
+
+function makeBright() {
+  foto.makeBright();
+}
+
+function makeDark() {
+  foto.makeDark();
+}
+
+function makeBlur() {
+  foto.applyBlurFilter();
+}
+
+function makeEmboss() {
+  foto.applyEmbossFilter();
+}
+
+function makeSharp() {
+  foto.applySharpFilter();
+  
+}
+
+function download() {
+  foto.export();
+}
+
+function openColorpicker() {
+  document.getElementById("color-picker").click();
+}
+
+function makeColorize(elem) {
+  var color = elem.value;
+  foto.colorize(color);
+}
