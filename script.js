6fd819935b37ea504fb5dcb20b3cdf0707a3130d
Hero (Angel Pena) <44003445+HeroofLight555@users.noreply.github.com>
2020-11-03 18:24:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -67,6 +67,39 @@ function makeTransparent() {
   foto.makeTransparent();
   
 }
-function Saturation(t) {
-  document.getElementById("foto-image").style.filter = `saturate(${t}%)`;
+function Saturation(value){
+
+    var i, j;
+    var t = foto.imageData;
+
+    var width = foto.imageWidth, height = foto.imageHeight;
+
+    if (value === undefined){
+      value = 1.0;
+    }
+    var satLevel = value;
+    var redLumConst = 0.3086;
+    var greenLumConst = 0.6094;
+    var blueLumConst = 0.0820;
+    var a = (1 - satLevel) * redLumConst + satLevel
+    var b = (1 - satLevel) * redLumConst;
+    var c = (1 - satLevel) * redLumConst;
+    var d = (1 - satLevel) * greenLumConst;
+    var e = (1 - satLevel) * greenLumConst + satLevel;
+    var f = (1 - satLevel) * greenLumConst;
+    var g = (1 - satLevel) * blueLumConst;
+    var h = (1 - satLevel) * blueLumConst;
+    var i = (1 - satLevel) * blueLumConst + satLevel;
+    for (i = 0; i < height; i++){
+      for(j  = 0; j < width; j++){
+        var pix = (i * width + j) * 4;
+        t.data[pix]   = a * t.data[pix] + d * t.data[pix + 1] + g * t.data[pix + 2];
+				t.data[pix + 1] = b * t.data[pix] + e * t.data[pix + 1] + h * t.data[pix + 2];
+				t.data[pix + 2]  = c * t.data[pix] + f * t.data[pix + 1] + i * t.data[pix + 2];
+      }
+    }
+
+    foto.operationEditedCtx.putImageData(t,0,0);
+    
+    foto.previewImage();
 }
